053a9389bde88e5048dad0c9675a11fad85c1ed5
Isabele Akemi <70545832+isabeleakemi@users.noreply.github.com>
2023-02-18 03:00:33 -0300

    busca api

--- src/views/Clientes.js
+++ src/views/Clientes.js
@@ -2,8 +2,22 @@ import React, { Component } from "react";
 import Footer from './shared/Footer';
 import Header from './shared/Header';
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-class Clientes extends Component {
+class Clientes extends Component {  
+  state = {
+    clientes: []
+  }
+
+  componentDidMount(){
+    axios.get('https://localhost:44367/clientes').then(response => {
+      
+      const clientes = response.data
+      this.setState({ clientes })
+      
+    })
+  }
+
   render() {
       return (
         <div>
@@ -13,15 +27,24 @@ class Clientes extends Component {
               <div className="container-xxl bd-gutter">
                 <div className="col-md-8 mx-auto text-center">
                   <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png" width="200" height="165" alt="Bootstrap" className="d-block mx-auto mb-3"></img>
-                  <h1 className="mb-3 fw-semibold lh-1">Lista de clientes</h1>
-                  <ul>
-                    <li>
-                      <Link to="/cliente/1">Cliente 1</Link>
-                    </li>
-                    <li>
-                      <Link to="/cliente/2">Cliente 2</Link>
-                    </li>
-                  </ul>
+                  <h1 className="mb-3 fw-semibold lh-1">Lista de clientes - {this.state.clientes.length}</h1>
+                  <table className="table">
+                    <tr>
+                      <th>Nome</th>
+                      <th>Telefone</th>
+                      <th>Endereço</th>
+                      <th></th>
+                    </tr>
+                  
+                    {this.state.clientes.map(cliente => (
+                      <tr>
+                        <td>{cliente.nome}</td>
+                        <td>{cliente.telefone}</td>
+                        <td>{cliente.endereco}</td>
+                        <td><Link to={`/cliente/${cliente.id}`}>Editar</Link></td>
+                      </tr>
+                    ))}
+                  </table>
                 </div>
               </div>
             </div>
